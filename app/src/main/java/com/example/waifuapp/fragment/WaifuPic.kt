0b7262988b799102be57e5ffb3c2d20430768715
mfitrie <mfitrie78@gmail.com>
2022-02-23 00:06:33 +0800

    changes 2 after revert

--- app/src/main/java/com/example/waifuapp/fragment/WaifuPic.kt
+++ app/src/main/java/com/example/waifuapp/fragment/WaifuPic.kt
@@ -22,10 +22,7 @@ import com.example.waifuapp.ViewModel.MainViewModel
 import com.example.waifuapp.ViewModel.MainViewModelProviderFactory
 import com.example.waifuapp.R
 import com.example.waifuapp.Repository.Repository
-import com.example.waifuapp.model.AnimeQuote.AnimeQuoteData
-import com.example.waifuapp.model.Waifu.WaifuJson
 import kotlinx.android.synthetic.main.fragment_waifu_pic.*
-import retrofit2.Response
 import java.util.jar.Manifest
 
 class WaifuPic : Fragment() {
@@ -54,8 +51,7 @@ class WaifuPic : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
 
-
-        initWaifuApp(viewModel)
+        getPic(viewModel)
 
         btn_get.setOnClickListener {
             getPic(viewModel)
@@ -83,8 +79,6 @@ class WaifuPic : Fragment() {
     }
 
 
-
-
     //    Context.CONNECTIVITY_SERVICE
     @RequiresApi(Build.VERSION_CODES.M)
     private fun checkInternetConnection(): Boolean {
@@ -110,46 +104,45 @@ class WaifuPic : Fragment() {
 
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun initWaifuApp(viewModel: MainViewModel) {
-        Log.d("INIT_WAIFU", "initWaifuApp: called")
-        if (checkInternetConnection()) {
-            viewModel.getWaifu()
-
-
-            // waifu pic
-            val dataObserver  = getObserverWaifu(viewModel, true)
-            viewModel.waifuResponse.observe(viewLifecycleOwner, dataObserver)
-            viewModel.waifuResponse.removeObserver(dataObserver)
-
-
-            // Anime quote
-            val observerAnimeQuote = getQuote(viewModel)
-            viewModel.animeQuoteResponse.observe(viewLifecycleOwner, observerAnimeQuote)
-            viewModel.animeQuoteResponse.removeObserver(observerAnimeQuote)
-
-        } else {
-            Toast.makeText(requireContext(), "Please turn on your internet connection",Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.M)
     private fun getPic(viewModel: MainViewModel) {
+        progressBar.visibility = View.VISIBLE
+        Log.d("PROGRESS_BAR", "getPic: progressbar visibile")
+
         if (checkInternetConnection()) {
             viewModel.getWaifu()
+            viewModel.waifuResponse.observe(viewLifecycleOwner, Observer { response ->
+                if (response.isSuccessful) {
+
+                    // test code
+//                    val data = response.body()?.images.toString().split(", ")[12].split("url=")[1].split(")]")[0]
+                    data = response.body()?.images?.get(0)?.url.toString()
+                    Log.d("RESPONSE", data.toString())
+                    Glide.with(this).load(data).into(im_WaifuPic)
+
+                    progressBar.visibility = View.GONE
+                    Log.d("PROGRESS_BAR", "getPic: progressbar gone")
 
 
-            // waifu pic
-            val dataObserver  = getObserverWaifu(viewModel, true)
-            viewModel.waifuResponse.observe(viewLifecycleOwner, dataObserver)
-            viewModel.waifuResponse.removeObserver(dataObserver)
 
 
-            // Anime quote
-            val observerAnimeQuote = getQuote(viewModel)
-            viewModel.animeQuoteResponse.observe(viewLifecycleOwner, observerAnimeQuote)
-            viewModel.animeQuoteResponse.removeObserver(observerAnimeQuote)
+                } else {
+                    Toast.makeText(context, response.code(), Toast.LENGTH_SHORT).show()
+                    Log.d("FAILED", "getPic: ${response.code()}")
+                }
+            })
+
+
+            // Anime Quote
+            viewModel.getAnimeQuote()
+            viewModel.animeQuoteResponse.observe(viewLifecycleOwner, Observer { response ->
+                if(response.isSuccessful){
+                    val data = response.body()?.quote
+                    tv_animeQuote.text = data.toString()
+                }else{
+                    tv_animeQuote.text = "We can't waste time Worrying about the what if's"
+                }
+            })
+
 
         } else {
             Toast.makeText(requireContext(), "Please turn on your internet connection",Toast.LENGTH_SHORT).show()
@@ -163,18 +156,39 @@ class WaifuPic : Fragment() {
         progressBar.visibility = View.VISIBLE
         Log.d("PROGRESS_BAR", "getPic: progressbar visibile")
 
-
         if (checkInternetConnection()) {
+            viewModel.getWaifuGif(true)
+            viewModel.waifuResponseGif.observe(viewLifecycleOwner, Observer { response ->
+                if (response.isSuccessful) {
 
-            // get gif
-            val dataObserver  = getObserverWaifu(viewModel, false)
-            viewModel.waifuResponseGif.observe(viewLifecycleOwner, dataObserver)
-            viewModel.waifuResponseGif.removeObserver(dataObserver)
+                    // test code
+//                    val data = response.body()?.images.toString().split(", ")[12].split("url=")[1].split(")]")[0]
 
-            // Anime quote
-            val observerAnimeQuote = getQuote(viewModel)
-            viewModel.animeQuoteResponse.observe(viewLifecycleOwner, observerAnimeQuote)
-            viewModel.animeQuoteResponse.removeObserver(observerAnimeQuote)
+                    Log.d("INSIDE_GIF_SCOPE", "getGif: Im clicked")
+                    data = response.body()?.images?.get(0)?.url.toString()
+                    Log.d("RESPONSE", data.toString())
+                    Glide.with(this).load(data).into(im_WaifuPic)
+
+                    progressBar.visibility = View.GONE
+                    Log.d("PROGRESS_BAR", "getPic: progressbar gone")
+
+
+                } else {
+                    Toast.makeText(requireContext(), response.code(), Toast.LENGTH_SHORT).show()
+                }
+            })
+
+
+            // Anime Quote
+            viewModel.getAnimeQuote()
+            viewModel.animeQuoteResponse.observe(viewLifecycleOwner, Observer { response ->
+                if(response.isSuccessful){
+                    val data = response.body()?.quote
+                    tv_animeQuote.text = data.toString()
+                }else{
+                    tv_animeQuote.text = "We can't waste time Worrying about the what if's"
+                }
+            })
 
 
 
@@ -190,87 +204,12 @@ class WaifuPic : Fragment() {
     }
 
 
-    private fun getObserverWaifu(viewModel: MainViewModel, isPic: Boolean): Observer<Response<WaifuJson>>{
-        if(isPic){
-            viewModel.getWaifu()
-            return Observer<Response<WaifuJson>> { response ->
-                progressBar.visibility = View.VISIBLE
-                Log.d("PROGRESS_BAR", "getPic: progressbar visibile")
-
-                if (response.isSuccessful) {
-                    data = response.body()?.images?.get(0)?.url.toString()
-                    Log.d("RESPONSE", data.toString())
-                    Glide.with(this).load(data).into(im_WaifuPic)
-
-                    progressBar.visibility = View.GONE
-                    Log.d("PROGRESS_BAR", "getPic: progressbar gone")
-
-                }
-                else if(response.code() == 404){
-                    Toast.makeText(context, "Fail to get Pic", Toast.LENGTH_SHORT).show()
-                }
-                else {
-                    Toast.makeText(context, response.code(), Toast.LENGTH_SHORT).show()
-                    Log.d("FAILED", "getPic: ${response.code()}")
-                }
-            }
-        }else{
-            Log.d("ELSE", "getObserverWaifu: IN ELSE")
-            progressBar.visibility = View.VISIBLE
-            Log.d("PROGRESS_BAR", "getPic: progressbar visibile")
-
-            viewModel.getWaifuGif(true)
-            return Observer<Response<WaifuJson>> { response ->
-                if (response.isSuccessful) {
-
-                    Log.d("INSIDE_GIF_SCOPE", "getGif: Im clicked")
-                    data = response.body()?.images?.get(0)?.url.toString()
-                    Log.d("RESPONSE", data.toString())
-                    Glide.with(this).load(data).into(im_WaifuPic)
-
-                    progressBar.visibility = View.GONE
-                    Log.d("PROGRESS_BAR", "getPic: progressbar gone")
-
-
-                }else if(response.code() == 404){
-                    Toast.makeText(context, "Fail to get Gif", Toast.LENGTH_SHORT).show()
-                }
-                else {
-                    Toast.makeText(requireContext(), response.code(), Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-
-
-
-
     @RequiresApi(Build.VERSION_CODES.M)
     private fun downloadImage(url: String) {
         if (checkInternetConnection()) {
             viewModel.downloadWaifu(url)
         }
     }
-
-
-
-    // FUNCTION inside
-    // Anime Quote function
-    private fun getQuote(viewModel: MainViewModel): Observer<Response<AnimeQuoteData>>{
-        viewModel.getAnimeQuote()
-        return Observer<Response<AnimeQuoteData>> { response ->
-            if(response.isSuccessful){
-                val data = response.body()?.quote
-                tv_animeQuote.text = data.toString()
-                Log.d("GET_ANIME_QUOTE", "quote")
-            }else{
-                tv_animeQuote.text = "We can't waste time Worrying about the what if's"
-            }
-        }
-    }
-
-
 
 
 
